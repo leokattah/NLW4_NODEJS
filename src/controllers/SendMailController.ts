70c577c3988f2dc1bf4bcc0ec2efdcf41f87de61
leokattah <leokattah@gmail.com>
2021-02-25 11:42:45 -0300

    readme changed

---
+++ src/controllers/SendMailController.ts
@@ -0,0 +1,10 @@
+import {Request , Response} from 'express';
+
+class SendMailController {
+  async execute(request: Request , response:Response ){
+const {email, survey_id} = request.body;
+const usersRepository = 
+  }
+} 
+
+export {SendMailController}
