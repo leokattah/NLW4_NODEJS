184fba6f1a4a9e27504036bfc73f7445d547ce2e
leokattah <leokattah@gmail.com>
2021-02-25 09:31:25 -0300

    tests finished

--- src/__tests__/User.test.ts
+++ src/__tests__/User.test.ts
@@ -1,22 +1,40 @@
-
-
+ 
 import request from 'supertest';
 import { app } from '../app';
+
 import createConnection from '../database';
 
-describe("Surveys", async () => {
+
+describe("Users", () => {
+
+    // Antes de tudo, criar as migrations para o banco de dados test ter tabelas
     beforeAll(async () => {
         const connection = await createConnection();
         await connection.runMigrations();
     });
 
-    it("Should ba able to create a new survey", async () => {
-        const response = await request(app).post("/surveys").send({
-            title: "Title Example",
-            description: "Description Example",
+    it("Should be able to create a new user", async () => {
+        // Criando uma requisicao pela biblioteca supertest
+        const response = await request(app).post("/users")
+        .send({
+            email: "user@example.com",
+            name: "User Example",
         });
 
+        // Espara-se que seja 201 pois assim o test funcionou e criou o usuário
         expect(response.status).toBe(201);
+    });
+
+    it("Should not be able to create a user with exists email", async () => {
+        // Criando uma requisicao pela biblioteca supertest
+        const response = await request(app).post("/users")
+        .send({
+            email: "user@example.com",
+            name: "User Example",
+        });
+
+        // Espara-se que seja 400 pois vai dar erro ao criar usuario com email duplicado
+        expect(response.status).toBe(400);
     });
 
 });
